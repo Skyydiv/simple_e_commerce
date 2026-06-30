9c58282531bda643ee0681fa29e5f660550ea6cb
21135945 <Corentin.Fontaine.1@etu.sorbonne-universite.fr>
2026-06-30 17:06:31 +0200

    add a method to build an example list of users

--- src/main/java/com/learning/simple_e_commerce/entity/User.java
+++ src/main/java/com/learning/simple_e_commerce/entity/User.java
@@ -6,6 +6,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,4 +22,48 @@ public class User {
     private Role role;
 
 
+    public static User buildExempleUser(Role role){
+        return builder()
+                .user_id(1L)
+                .first_name("John")
+                .last_name("Doe")
+                .email("mail@hotmail.com")
+                .phone("1234567890")
+                .address("123 Main St")
+                .role(role)
+                .build();
+    }
+
+    public static List<User> buildExempleUserList(){
+        return List.of(
+                buildExempleUser(Role.ADMIN),
+                builder()
+                        .user_id(2L)
+                        .first_name("Jane")
+                        .last_name("Smith")
+                        .email("jane.smith@hotmail.com")
+                        .phone("1234567891")
+                        .address("124 Main St")
+                        .role(Role.USER)
+                        .build(),
+                builder()
+                        .user_id(3L)
+                        .first_name("Bob")
+                        .last_name("Brown")
+                        .email("bob.brown@hotmail.com")
+                        .phone("1234567892")
+                        .address("125 Main St")
+                        .role(Role.USER)
+                        .build(),
+                builder()
+                        .user_id(4L)
+                        .first_name("Alice")
+                        .last_name("Green")
+                        .email("alice.green@hotmail.com")
+                        .phone("1234567893")
+                        .address("126 Main St")
+                        .role(Role.USER)
+                        .build()
+        );
+    }
 }
